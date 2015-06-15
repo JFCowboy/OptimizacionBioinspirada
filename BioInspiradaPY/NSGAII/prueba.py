18da1bf4c20f773f5f17a6d26bc3312e2f401d6a
JFCowboy <cowboy9209@gmail.com>
2015-06-14 23:46:59 -0500

    Correccion Evaluate
    Corrección del Evaluate para que sea compatible

--- BioInspiradaPY/NSGAII/prueba.py
+++ BioInspiradaPY/NSGAII/prueba.py
@@ -1,5 +1,21 @@
 '''
 Created on 14/06/2015
 
-@author: JuanFelipe
+@author: JuanFelipe, Lin
 '''
+import numpy as np
+from FirstGen import genFirst
+from Evaluate import evaluate
+
+
+n = 10
+m = 2
+inv = 20
+pf = np.array([np.random.randint(100, size=n) for x in xrange(m)])
+cc = np.random.randint(100, 150, size=m)
+ww = np.random.randint(50, size=n)
+pg = genFirst(inv, n, m)
+
+print "PG:",pg, "WW:",ww, "CC:",cc, "PF:",pf
+
+print evaluate(pg, pf, ww, cc, n, m)
